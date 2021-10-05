55f4ec69bd111399f86cc2020b3aee1075d0ae70
Ale Bullo <bullorinia@gmail.com>
2021-10-05 19:24:05 -0300

    first Commit, logo and quick edits

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          GameStock: Videojuegos a tu alcance
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Alex-Bulo/gamestock-bullorini.git"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React Repo
         </a>
       </header>
     </div>
